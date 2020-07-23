761e386d832cb13339511b92204bbaf541fcc8b8
maolele <m01.sgwk@gmail.com>
2020-07-23 20:54:01 +0900

    Add caluculation of month and date

--- js/main.js
+++ js/main.js
@@ -1,4 +1,120 @@
 'use strict';
+console.clear();
 {
-  
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+
+  //get last month date
+  function getCalenderHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const num = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < num; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    return dates;
+
+  }
+
+  //get this month date
+  function getCalenderBody() {
+    const dates = [];
+    const lastDate = new Date(year, month + 1, 0).getDate();
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+    return dates;
+  }
+
+  //get next month date
+  function getCalenderTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    return dates;
+  }
+
+  function clearTitle() {
+    const tbody = document.querySelector('tbody');
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+  }
+
+  function renderTitle() {
+    const title = `${year}/${String(month + 1).padStart(2, '0')}`;
+    document.getElementById('title').textContent = title;
+  }
+
+  function renderWeeks() {
+    const dates = [
+      ...getCalenderHead(),
+      ...getCalenderBody(),
+      ...getCalenderTail(),
+    ];
+    const weeks = [];
+    const weeksCount = dates.length / 7;
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0, 7)); //先頭から７個分削除してpush
+    }
+
+    weeks.forEach(week => {
+      const tr = document.createElement('tr');
+      week.forEach(date => {
+        const td = document.createElement('td');
+        td.textContent = date.date;
+        if (date.isToday) {
+          td.classList.add('today');
+        }
+        if (date.isDisabled) {
+          td.classList.add('disabled');
+        }
+        tr.appendChild(td);
+      });
+      document.querySelector('tbody').appendChild(tr);
+    });
+  }
+
+  function createCalender() {
+    clearTitle();
+    renderTitle();
+    renderWeeks();
+  }
+
+  createCalender();
+
+  document.getElementById('prev').addEventListener('click', () => {
+    month--;
+    if (month < 0) {
+      year--;
+      month = 11;
+    }
+    createCalender();
+  });
+
+  document.getElementById('next').addEventListener('click', () => {
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+    createCalender();
+  });
+
 }
